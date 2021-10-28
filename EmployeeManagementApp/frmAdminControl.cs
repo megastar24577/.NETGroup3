04f78a9aa4c4d111f2a98d4d5855ac6ccd1ee41f
Phuoc Thanh <megastar24577@gmail.com>
2021-10-28 23:09:12 +0700

    Minimize form, adding and deleted unescessary form

---
+++ EmployeeManagementApp/frmAdminControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeManagementApp
+{
+    public partial class frmAdminControl : UserControl
+    {
+        //Singleton
+        private static frmAdminControl _instance;
+
+        public static frmAdminControl Instance
+        {
+            get
+            {
+                if (_instance is null)
+                {
+                    _instance = new frmAdminControl();
+                }
+
+                return _instance;
+            }
+        }
+        public frmAdminControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- EmployeeManagementApp/frmManagerDetail.cs
+++ EmployeeManagementApp/frmManagerDetail.cs
@@ -1,5 +1,4 @@
-﻿using BusinessObject;
-using DataAccess.Repository;
+﻿using DataAccess.Repository;
 using System;
 using System.Windows.Forms;
 
@@ -11,57 +10,16 @@ namespace MyStoreWinApp
         {
             InitializeComponent();
         }
-
-        //----------------------------------------------------
-        public IMemberRepository MemberRepository { get; set; }
-        public bool InsertOrUpdate { get; set; }
-        public MemberObject MemberInfo { get; set; }
-
-        //----------------------------------------------------
+        
 
         private void FrmMemberDetail_Load(object sender, EventArgs e)
         {
-            txtManagerMemberID.Enabled = !InsertOrUpdate;
-            if(InsertOrUpdate == true)
-            {
-                txtManagerMemberID.Text = MemberInfo.MemberId.ToString();
-                txtManagerCountry.Text = MemberInfo.CompanyName;
-                txtManagerName.Text = MemberInfo.Email;
-                txtManagerPassword.Text = MemberInfo.Password;
-                txtCity.Text = MemberInfo.City;
-                txtManagerEmail.Text = MemberInfo.Country;
-            }
+            
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            try
-            {
-                var Member = new MemberObject
-                {
-                    MemberId = int.Parse(txtManagerMemberID.Text),
-                    CompanyName = txtManagerCountry.Text,
-                    Password = txtManagerPassword.Text,
-                    Email = txtManagerName.Text,
-                    City = txtCity.Text,
-                    Country = txtManagerEmail.Text
-                };
-                Console.WriteLine("Form member detail: " + Member);
-                if(InsertOrUpdate == false)
-                {
-                    MemberRepository.InsertMember(Member);
-                }
-                else
-                {
-                    MemberRepository.Update(Member);
-                }
-                Close();
-            }
-
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, InsertOrUpdate == false ? "Add Member" : "Update a Member");
-            }
+            
         }
 
         private void btnCancel_Click(object sender, EventArgs e) => Close();

e2c0df12a832fcd340c13bc7eab2f8f40693e765
Hiếu Trần <hieutran1736@gmail.com>
2021-10-30 13:38:38 +0700

    update form manager

---
+++ EmployeeManagementApp/frmManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeManagementApp
+{
+    public partial class frmManager : Form
+    {
+        public frmManager()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPhone_TextChanged(object sender, EventArgs e)
+        {
+                    }
+    }
+}
